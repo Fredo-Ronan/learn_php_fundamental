76107797a1e50b6702bbb8cf7b9714e676e30fcb
Fredo-Ronan <fredocode06@gmail.com>
2023-09-17 18:14:16 +0700

    👍 finish all files not tested yet

--- GD3_B_11446/index.php
+++ GD3_B_11446/index.php
@@ -142,6 +142,16 @@ $gambar = [
         </div>
         </div>
 
+        <div class="container">
+            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+                <div class="col-md-4 d-flex align-items-center">
+                    <span class="mb-3 mb-md-0 text-body-secondary">© 2023 <?php echo $detail["name"];?></span>
+                </div>
+            </footer>
+        </div>
     </main>
+
+    <script src="./assets/css/bootstrap.min.js"></script>
+    <script src="./assets/js/home-nav.js"></script>
 </body>
 </html>
